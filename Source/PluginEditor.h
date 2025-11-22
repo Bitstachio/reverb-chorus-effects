da5f57292492c6b3d8c805ea4a4ee442b2e81f97
BarbodH <habibi.k.barbod@gmail.com>
2025-11-22 03:13:42 -0500

    Add & apply clang-format

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -13,35 +13,34 @@
 
 //==============================================================================
 /**
-*/
-class A3AudioProcessorEditor  : public juce::AudioProcessorEditor
-{
+ */
+class A3AudioProcessorEditor : public juce::AudioProcessorEditor {
 public:
-    A3AudioProcessorEditor (A3AudioProcessor&);
+    A3AudioProcessorEditor(A3AudioProcessor &);
     ~A3AudioProcessorEditor() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics &) override;
     void resized() override;
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
-    A3AudioProcessor& audioProcessor;
-    
-    juce::Slider cutOffSlider;
-    juce::Slider rateSlider;
-    juce::Slider depthSlider;
-    juce::Slider gainSlider;
+    A3AudioProcessor &audioProcessor;
+
+    juce::Slider   cutOffSlider;
+    juce::Slider   rateSlider;
+    juce::Slider   depthSlider;
+    juce::Slider   gainSlider;
     juce::ComboBox filterMenu;
     juce::ComboBox phaserMenu;
-    
-    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> cutOffValue;
-    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> rateValue;
-    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> depthValue;
-    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> gainValue;
+
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment>   cutOffValue;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment>   rateValue;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment>   depthValue;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment>   gainValue;
     std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> filterMenuValue;
     std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> phaserMenuValue;
-    
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (A3AudioProcessorEditor)
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(A3AudioProcessorEditor)
 };
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -12,61 +12,60 @@
 
 //==============================================================================
 /**
-*/
-class A3AudioProcessor  : public juce::AudioProcessor
-{
+ */
+class A3AudioProcessor : public juce::AudioProcessor {
 public:
     //==============================================================================
     A3AudioProcessor();
     ~A3AudioProcessor() override;
 
     //==============================================================================
-    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
 
-   #ifndef JucePlugin_PreferredChannelConfigurations
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
-   #endif
+#ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported(const BusesLayout &layouts) const override;
+#endif
 
-    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+    void processBlock(juce::AudioBuffer<float> &, juce::MidiBuffer &) override;
 
     //==============================================================================
-    juce::AudioProcessorEditor* createEditor() override;
-    bool hasEditor() const override;
+    juce::AudioProcessorEditor *createEditor() override;
+    bool                        hasEditor() const override;
 
     //==============================================================================
     const juce::String getName() const override;
 
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
+    bool   acceptsMidi() const override;
+    bool   producesMidi() const override;
+    bool   isMidiEffect() const override;
     double getTailLengthSeconds() const override;
 
     //==============================================================================
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const juce::String getProgramName (int index) override;
-    void changeProgramName (int index, const juce::String& newName) override;
+    int                getNumPrograms() override;
+    int                getCurrentProgram() override;
+    void               setCurrentProgram(int index) override;
+    const juce::String getProgramName(int index) override;
+    void               changeProgramName(int index, const juce::String &newName) override;
 
     //==============================================================================
-    void getStateInformation (juce::MemoryBlock& destData) override;
-    void setStateInformation (const void* data, int sizeInBytes) override;
-    
-    void updateFX();
-    void updateParameters ();
+    void getStateInformation(juce::MemoryBlock &destData) override;
+    void setStateInformation(const void *data, int sizeInBytes) override;
+
+    void                               updateFX();
+    void                               updateParameters();
     juce::AudioProcessorValueTreeState apvts;
 
 private:
     juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
-    
+
     juce::dsp::StateVariableTPTFilter<float> stateVariableFilter;
-    bool bypassFilter = false;
-    
+    bool                                     bypassFilter = false;
+
     enum { phaserIndex, gainIndex };
-    juce::dsp::ProcessorChain<juce::dsp::Phaser<float>,juce::dsp::Gain<float>> fxChain;
-    bool bypassPhaser = false;
-    
+    juce::dsp::ProcessorChain<juce::dsp::Phaser<float>, juce::dsp::Gain<float>> fxChain;
+    bool                                                                        bypassPhaser = false;
+
     //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (A3AudioProcessor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(A3AudioProcessor)
 };
